44c63e6f9827c10a24db9d115295b2a54abb18d1
Isadora-Gomes <isasilvacpv@gmail.com>
2025-01-30 13:43:07 -0300

    pag alu e inst

---
+++ pagAluno.php
@@ -0,0 +1,51 @@
+<?php
+    include 'php/conexao.php';
+
+    $sql_alunos= "SELECT aluno_nome, aluno_cpf, aluno_endereco, aluno_telefone FROM alunos";
+    
+    $resultado_alunos = $conexao->query($sql_alunos);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alunos</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="pagAluno.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Alunos cadastrados</h1>
+        <div class="tabela-div">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>CPF</th>
+                        <th>Telefone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($resultado_alunos->num_rows > 0) {
+                        while($linha = $resultado_alunos->fetch_assoc()){
+                            echo "<tr>";
+                            echo "<td>" . $linha['aluno_nome'] . "</td>";
+                            echo "<td>" . $linha['aluno_cpf'] . "</td>";
+                            echo "<td>" . $linha['aluno_telefone'] . "</td>";
+                            echo "<tr>";
+                        }                   
+                    }
+                        ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
---
+++ pagInstrutor.php
@@ -0,0 +1,49 @@
+<?php
+    include 'php/conexao.php';
+
+    $sql_instrutores= "SELECT instrutor_nome, instrutor_especialidade FROM instrutores";
+    
+    $resultado_instrutores = $conexao->query($sql_instrutores);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instrutores</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="pagAluno.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Instrutores cadastrados</h1>
+        <div class="tabela-div">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Especialidade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($resultado_instrutores->num_rows > 0) {
+                        while($linha = $resultado_instrutores->fetch_assoc()){
+                            echo "<tr>";
+                            echo "<td>" . $linha['instrutor_nome'] . "</td>";
+                            echo "<td>" . $linha['instrutor_especialidade'] . "</td>";
+                            echo "<tr>";
+                        }                   
+                    }
+                        ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
